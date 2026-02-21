1f9432b1ab5a54ecdd953ab958097e6e6a435221
Bhavin Hadiya <bhavinhadiya386@gmail.com>
2026-02-21 17:08:38 +0530

    ....

--- CE01.java
+++ CE01.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class CE01 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter distance in meters: ");
-        double meters = input.nextDouble();
+        double meters = sc.nextDouble();
         double feet = meters * 3.28084;
         System.out.printf("Distance in feet: %.2f\n", feet);
-        input.close();
     }
 }
+
