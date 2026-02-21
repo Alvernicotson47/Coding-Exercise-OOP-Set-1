b2e191c9155aafa7f4d0b3089e0eba5e863a9de9
Bhavin Hadiya <bhavinhadiya386@gmail.com>
2026-02-21 17:13:56 +0530

    ....
    Correct variable names for weight and height calculations.

--- CE04.java
+++ CE04.java
@@ -7,9 +7,10 @@ public class CE04 {
         double pounds = sc.nextDouble();
         System.out.print("Enter height in inches: ");
         double inches = sc.nextDouble();
-        double weightKg = pounds * 0.45359237;
-        double heightMeters = inches * 0.0254;
-        double bmi = weightKg / (heightMeters * heightMeters);
-        System.out.printf("Your BMI is: %.2f\n", bmi);
+        double weightInKg = pounds * 0.45359237;
+        double heightInMeters = inches * 0.0254;
+        double b = weightKg / (heightInMeters * heightInMeters);
+        System.out.printf("Your BMI is: %.2f\n", b);
     }
 }
+
